277bfae9601f9cd5f2c7c2a98a8dc42485984c26
Dtompanda <thewalkerdarkknight@gmail.com>
2023-06-05 15:43:28 +0530

    Bugs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+<<<<<<< HEAD
 import express from 'express';
 import cors from 'cors'
 
 //Consts & vars
 const PORT=3500
+=======
+import express from 'express'
+import cors from 'cors'
+
+//VARS CONSTANTS
+
+>>>>>>> 462fdbc (First commit)
 const app=express()
 
 //Middlewares
 
 app.use(cors())
+<<<<<<< HEAD
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
@@ -20,3 +29,14 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(PORT,()=>console.log(`Running @ ${PORT}`))
+=======
+
+
+//Routes
+
+app.get('/',(req,res)=>{
+	res.end("HELLO WORLD")
+})
+
+app.listen(5555,()=>console.log("Hello"))
+>>>>>>> 462fdbc (First commit)
