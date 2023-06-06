9ca7e18bfbfe0115f2d1999fe1406127478c2aeb
Dtompanda <thewalkerdarkknight@gmail.com>
2023-06-06 13:21:30 +0530

    Changed index.js&package.json

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
+
 import express from 'express';
 import cors from 'cors'
 
 //Consts & vars
 const PORT=3500
-=======
-import express from 'express'
-import cors from 'cors'
 
 //VARS CONSTANTS
-
->>>>>>> 462fdbc (First commit)
 const app=express()
 
 //Middlewares
 
 app.use(cors())
-<<<<<<< HEAD
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
@@ -29,7 +23,7 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(PORT,()=>console.log(`Running @ ${PORT}`))
-=======
+
 
 
 //Routes
@@ -39,4 +33,4 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(5555,()=>console.log("Hello"))
->>>>>>> 462fdbc (First commit)
+
